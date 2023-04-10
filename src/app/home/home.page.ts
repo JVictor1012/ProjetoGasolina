b492738993d355636e8364bb1acf1c8097923b4a
JOÃO VICTOR NUNES DOS SANTOS <20211148060004@ifrn.local>
2023-04-10 17:36:40 -0300

    Adicioando projeto ao repositório

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +9,43 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+
+
+  resultado : number;
+  color: String;
+  opcao: String
+
+  constructor() {
+
+    this.resultado = 0;
+    this.color = "light"
+    this.opcao = "Descubra o melhor preço para você!"
+  }
+
+  calcular(alcool: IonInput, gasolina: IonInput) {
+    let calculo: any;
+    if(alcool.value != null && gasolina.value != null){
+      calculo = +alcool.value / +gasolina.value;
+    }
+
+
+
+
+    this.resultado = calculo
+
+    console.log(this.resultado)
+
+    if(this.resultado <= 0.7){
+      this.color = "tertiary"
+      this.opcao = "Você deveria usar Álcool!"
+    }
+
+    if(this.resultado > 0.7){
+      this.color = "warning"
+      this.opcao = "Você deveria usar Gasolina!"
+
+    }
+
+  }
+
 }
